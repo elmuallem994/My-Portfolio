a55c18c25018a376c09fecfc17b105e9f606c2eb
almoallemlamas <elmuallem994@gmail.com>
2023-01-29 12:18:21 +0300

    portfolio yes

---
+++ src/constants/constants.js
@@ -0,0 +1,50 @@
+export const projects = [
+  {
+    title: "MERN Memories",
+    description:
+      "While building it you're going to learn many advanced React & JavaScript topics.",
+    image: "/images/1.png",
+    tags: ["Mongo", "Express", "React", "Node"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 0,
+  },
+  {
+    title: "E-Commerce",
+    description:
+      "While building it you're going to learn many advanced React & JavaScript topics.",
+    image: "/images/2.png",
+    tags: ["React", "JavaScript"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 1,
+  },
+  {
+    title: "WebRTC App",
+    description:
+      "While building it you're going to learn many advanced React & JavaScript topics.",
+    image: "/images/3.jpg",
+    tags: ["React", "WebRTC"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 2,
+  },
+  {
+    title: "Unichat",
+    description:
+      "While building it you're going to learn many advanced React & JavaScript topics.",
+    image: "/images/4.jpg",
+    tags: ["React", "ChatEngine", "Firebase"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
